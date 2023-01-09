c35e3648664bc2d31a4381c002caf980d5be3320
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-09 10:45:53 +0100

    Add solution

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,8 +1,39 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { StyledForm, StyledInputContainer } from "./Form.styled";
+import { useImmer } from "use-immer";
 
 export default function Form() {
-  const [mountain, setMountain] = useState({
+  // Updating with Spread Syntax
+
+  // const [mountain, setMountain] = useState({
+  //   name: "Mount Everest",
+  //   values: {
+  //     altitude: 8848,
+  //     mountainRange: "Himalayas",
+  //   },
+  // });
+
+  // function handleNameChange(event) {
+  //   setMountain({ ...mountain, name: event.target.value });
+  // }
+
+  // function handleAltitudeChange(event) {
+  //   setMountain({
+  //     ...mountain,
+  //     values: { ...mountain.values, altitude: event.target.value },
+  //   });
+  // }
+
+  // function handleMountainRangeChange(event) {
+  //   setMountain({
+  //     ...mountain,
+  //     values: { ...mountain.values, mountainRange: event.target.value },
+  //   });
+  // }
+
+  // Bonus: Updating with the `useImmer` hook
+
+  const [mountain, updateMountain] = useImmer({
     name: "Mount Everest",
     values: {
       altitude: 8848,
@@ -10,11 +41,23 @@ export default function Form() {
     },
   });
 
-  function handleNameChange(event) {}
+  function handleNameChange(event) {
+    updateMountain((draft) => {
+      draft.name = event.target.value;
+    });
+  }
 
-  function handleAltitudeChange(event) {}
+  function handleAltitudeChange(event) {
+    updateMountain((draft) => {
+      draft.values.altitude = event.target.value;
+    });
+  }
 
-  function handleMountainRangeChange(event) {}
+  function handleMountainRangeChange(event) {
+    updateMountain((draft) => {
+      draft.values.mountainRange = event.target.value;
+    });
+  }
 
   return (
     <StyledForm>
